25033b516b2e6013fd6aeeab7d5ebe3fccc45361
Rafael Eduardo <rafapaci@hotmail.com>
2019-07-29 22:33:18 -0300

    Novas dependências, Primeiro model Criado

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 // Iniciando o App
 const app = express()
 
 // Iniciando o DB
-mongoose.connect('mongodb+srv://@cluster0-qrvrm.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
+mongoose.connect(
+    'Link de conexão', 
+    { useNewUrlParser: true }
+)
+requireDir('./src/models')
+
+const Product = mongoose.model('Product')
 
 // Primeira rota
 app.get("/", (req, res) => {
-    res.send("Hello RafaWorks")
+    Product.create({
+        title: 'RafaWorks Test ♥',
+        description: 'Oh yeah!',
+        url: 'https://rafaworks.com.br'
+    });
+    return res.send("Hello RafaWorks")
 })
 
 app.listen(3001)
