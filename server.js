4a0b78965f1e51a600972d5fe3a50309cfa8d51e
Rafael Eduardo <rafapaci@hotmail.com>
2019-07-29 21:44:15 -0300

    Mongoose adicionado

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
+// Iniciando o App
 const app = express()
 
+// Iniciando o DB
+mongoose.connect('mongodb+srv://@cluster0-qrvrm.mongodb.net/test?retryWrites=true&w=majority')
+
+// Primeira rota
 app.get("/", (req, res) => {
     res.send("Hello RafaWorks")
 })
